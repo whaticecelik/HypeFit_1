8317a79e139300889334dc77e46ffbe2cd15dbb7
Hatice <hatice.2004.celik@gmail.com>
2024-12-06 12:47:14 +0300

    27.dkda kaldım

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,1 +1,2 @@
 // uygulamada sayfaları ve navigasyon işlemlerini burada tanımlıcaz
+// routes/app_routes.dart ile hallettim
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 // açılış ekranı için
+import 'package:dotlottie_loader/dotlottie_loader.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:lottie/lottie.dart';
 
 class LoadingScreen extends StatelessWidget {
   const LoadingScreen({super.key});
@@ -8,48 +10,53 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: const Color.fromARGB(255, 255, 161, 170),
+      body: SizedBox.expand(
+        child: Column(
+
+
+        
         children: [
           // Logo ve yükleme göstergesi bölümü
-          Container(
-            width: double.infinity,
-            child: Column(
-              children: [
-                // Logo bölümü
-                Container(
-                  width: 150,
-                  height: 150,
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    fit: BoxFit.contain,
+          Expanded(
+            //width: double.infinity,
+              child: Column(
+                children: [
+                  // Logo bölümü
+                  Expanded(
+                    child: Container(
+                      width: 200,
+                      height: 200,
+                      child: Image.asset(
+                        'assets/images/logo.png',
+                        fit: BoxFit.contain,
+                      ),
+                    ),
                   ),
+               //CircularProgressIndicator(),
+
+
+                SizedBox(
+                  width: 200,
+                  child: DotLottieLoader.fromAsset("assets/motions/loading.lottie", 
+                      frameBuilder: (BuildContext ctx, DotLottie? dotlottie) {
+                    if(dotlottie != null){
+                      return Lottie.memory(dotlottie.animations.values.single);
+                    } else {
+                        return Container();
+                    }
+                  }),
                 ),
-                
-                const SizedBox(height: 30),
-                
-                // Yükleme ikonu
-                const Icon(
-                  CupertinoIcons.arrow_2_circlepath,
-                  size: 40,
-                  color: Colors.blue,
-                ),
-                
-                const SizedBox(height: 20),
-                
-                // Yükleniyor yazısı
-                const Text(
-                  'Yükleniyor...',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
+
+
+
+               SizedBox(height: 20),
+
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
